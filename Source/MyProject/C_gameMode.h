f675f13153bec38f9d6e57c81e94bbe8f58ae975
Stijn Verhoeven <12001732@student.pxl.be>
2021-11-28 18:00:46 +0100

    commit

---
+++ Source/MyProject/C_gameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "C_gameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API AC_gameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
